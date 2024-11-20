6e70171edc5e9413a58bab6d520550681ef71d54
SimMan123 <simone.manzari2@marconibari.edu.it>
2024-11-20 09:40:31 +0000

    vector6

--- main.cpp
+++ main.cpp
@@ -2,10 +2,28 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+ int n,i,imin;
+ cout<<"numero altleti";
+ cin>>n;
+ vector<string>nomi[n],naz[n];
+ vector<float>tem[n];
+ for(i=0,i<n,i++) {
+    cout<<"nome atleta";
+    cin>>nomi[i];
+    cout<<"nazionale";
+    cin>>naz[i];
+    cout<<"tempo";
+    cin>>tem[i];
+ }
+ imin=0;
+ for(i=0,i<n,i++) {
+    if ( tem[i]<tem[imin]) {
+        imin=i;
+    }
+ }
+ cout<<"vincitore"<< nomi[imin];
+ cout<<"nazionale"<< naz[imin];
+ cout<<"tempo"<< tem[imin];
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
